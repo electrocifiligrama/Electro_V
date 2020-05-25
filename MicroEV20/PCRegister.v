d323591cb2259a1bf1dbcce17a263a72edf1952e
Manuel Mollon <mmollon@itba.edu.ar>
2020-05-25 12:27:26 -0300

    sigo debugeo

--- MicroEV20/PCRegister.v
+++ MicroEV20/PCRegister.v
@@ -4,10 +4,8 @@ module PCRegister (  input inc,
 							output reg [10:0] PC);
 							
    always @ (posedge inc)
-		begin
 			if(preLoad)
 				PC <= nextAdd;
 			else
 				PC <= PC + 1;
-		end
 endmodule
