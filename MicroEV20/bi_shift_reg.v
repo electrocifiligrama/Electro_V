ddefacd794f4afc90d4e5a9a69db5cb063b2b4c8
LuFernandez <lu.gfernandez@gmail.com>
2020-05-29 12:23:40 -0300

    arregle el bidirectional shift register para que pueda funcionar como cola tmb

--- MicroEV20/bi_shift_reg.v
+++ MicroEV20/bi_shift_reg.v
@@ -1,14 +1,39 @@
-module bi_shift_reg (input in, input clk, input enb, input dir, input rstn, output reg [3:0] out);   
-   always @ (posedge clk)
+//module bi_shift_reg (input in, input clk, input enb, input dir, input rstn, output reg [3:0] out);   
+  module bi_shift_reg (input in, input clk, input enb, input dir, input rstn, 
+								output reg out3, 
+								output reg out2,
+								output reg out1,
+								output reg out0);   
+	always @ (posedge clk)
       if (!rstn)
-         out <= 0;
+		begin
+			out3 <= 0;
+		  out2 <= 0;
+		  out1 <= 0;
+		  out0 <= 0;
+		 end
       else begin
          if (enb)
             case (dir)
-              0 :  out <= {out[2:0], 1'b0};
-              1 :  out <= {in, out[3:1]};
+              0 : begin 	//pop
+				  //out <= {out[2:0], 1'b0};
+				  out3 <= out2;
+				  out2 <= out1;
+				  out1 <= out0;
+				  out0 <= 1'b0;
+					end
+              1 :  begin	//push
+				  //out <= {in, out[3:1]};
+				  out3 <= in;
+				  out2 <= out3;
+				  out1 <= out2;
+				  out0 <= out1;
+				  end
             endcase
          else
-            out <= out;
+            out3 <= out3;
+				out2 <= out2;
+				out1 <= out1;
+				out0 <= out0;
       end
 endmodule
