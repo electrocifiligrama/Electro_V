f0704e11c0239f2e06b33d06a44101a544cda31f
LuFernandez <lu.gfernandez@gmail.com>
2020-05-30 17:07:59 -0300

    a ver si funciona ret

--- MicroEV20/bi_shift_reg.v
+++ MicroEV20/bi_shift_reg.v
@@ -1,51 +1,37 @@
+// Code your design here
 //module bi_shift_reg (input in, input clk, input enb, input dir, input rstn, output reg [3:0] out);   
-  module bi_shift_reg ( input in, 
-								input clk, 
-								input enb, 
-								input dir, 
-								input rstn,	
+module bi_shift_reg ( input in, 
+								input bsr, //input enb, 
+								input ret, 	input rstn,	
 								output reg out3, 
 								output reg out2,
 								output reg out1,
 								output reg out0,
 								output reg lifoOut,
 								output reg fifoOut);   
-								
-	always @ (posedge clk)
-      if (!rstn)
-			begin
-				out3 <= 0;
-				out2 <= 0;
-				out1 <= 0;
-				out0 <= 0;
-			 end
-      else 
-			begin
-         if (enb)
-            case (dir)
-              0 : 
-						begin 	//pop
-						  lifoOut <=out3;
-						  out3 <= out2;
-						  out2 <= out1;
-						  out1 <= out0;
-						  out0 <= 0;
-						end
-              1 :  
-						begin	//push
-							fifoOut <= out0;
-							out0 <= out1;
-							out1 <= out2;
-							out2 <= out3;
-							out3 <= in;				  
-						end
-            endcase
-         else
-           begin
-				out3 <= out3;
-            out2 <= out2;
-            out1 <= out1;
-            out0 <= out0;
-           end
-      end
+			
+//  if (!rstn) begin
+//			out3 <= 0;
+//			out2 <= 0;
+//			out1 <= 0;
+//			out0 <= 0;
+//	end						
+  always @ (posedge bsr or posedge ret) 
+  if(bsr)
+		begin
+			//push
+			fifoOut <= out0;
+			out0 <= out1;
+			out1 <= out2;
+			out2 <= out3;
+			out3 <= in;	
+		end
+	else if(ret)
+    	begin 	//pop
+		  lifoOut <=out3;
+		  out3 <= out2;
+		  out2 <= out1;
+		  out1 <= out0;
+		  out0 <= 0;
+		end
 endmodule
