e5cb6b4278879f8bc95e77037ff26055be67d7b8
LuFernandez <lu.gfernandez@gmail.com>
2020-05-31 17:27:08 -0300

    stack arreglado, falta ver lo de 2 branches seguidos

--- MicroEV20/bi_shift_reg.v
+++ MicroEV20/bi_shift_reg.v
@@ -1,37 +1,36 @@
-// Code your design here
-//module bi_shift_reg (input in, input clk, input enb, input dir, input rstn, output reg [3:0] out);   
-module bi_shift_reg ( input in, 
-								input bsr, //input enb, 
-								input ret, 	input rstn,	
-								output reg out3, 
-								output reg out2,
-								output reg out1,
-								output reg out0,
-								output reg lifoOut,
-								output reg fifoOut);   
-			
-//  if (!rstn) begin
-//			out3 <= 0;
-//			out2 <= 0;
-//			out1 <= 0;
-//			out0 <= 0;
-//	end						
-  always @ (posedge bsr or posedge ret) 
-  if(bsr)
-		begin
-			//push
-			fifoOut <= out0;
-			out0 <= out1;
-			out1 <= out2;
-			out2 <= out3;
-			out3 <= in;	
-		end
-	else if(ret)
-    	begin 	//pop
-		  lifoOut <=out3;
-		  out3 <= out2;
-		  out2 <= out1;
-		  out1 <= out0;
-		  out0 <= 1'b0;
-		end
+  module bi_shift_reg ( input in, 
+								input clk, 
+								input ret,
+								input bsr,
+								output reg [3:0]out, 
+								//output reg out2,
+								//output reg out1,
+								//output reg out0,
+								output reg lifoOut);
+								//output reg fifoOut);   
+								
+	reg enb;
+
+	
+	
+	always @(posedge clk)
+	begin
+			enb <= 1;	
+	end
+	
+	always @ (ret or bsr)
+			if (enb)
+			begin
+				if(ret)
+					begin 	//pop
+					  out <= {out[2:0], 1'b0};
+					  lifoOut <= out[3];
+					end
+				else if (bsr)
+					begin	//push
+						//fifoOut <= out0;
+						out <= {in, out[3:1]};		  
+					end
+			end
 endmodule
+
