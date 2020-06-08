5b54c1a089174d89811e6a08c77c73421393cde5
LuFernandez <lu.gfernandez@gmail.com>
2020-06-08 16:47:32 -0300

    stack arreglado, volvimos a la version sincronica, andan 2 bsr seguidos

--- MicroEV20/bi_shift_reg.v
+++ MicroEV20/bi_shift_reg.v
@@ -3,34 +3,19 @@
 								input ret,
 								input bsr,
 								output reg [3:0]out, 
-								//output reg out2,
-								//output reg out1,
-								//output reg out0,
-								output reg lifoOut);
-								//output reg fifoOut);   
+								output reg TOS);   
 								
-	reg enb;
 
 	
-	
-	always @(posedge clk)
-	begin
-			enb <= 1;	
-	end
-	
-	always @ (ret or bsr)
-			if (enb)
-			begin
-				if(ret)
-					begin 	//pop
-					  out <= {out[2:0], 1'b0};
-					  lifoOut <= out[3];
-					end
-				else if (bsr)
-					begin	//push
-						//fifoOut <= out0;
-						out <= {in, out[3:1]};		  
-					end
-			end
+	always @ (posedge clk)
+			if(ret)
+				begin 	//pop
+				  out <= {out[2:0], 1'b0};
+				  TOS <= out[3];
+				end
+			else if (bsr)
+				begin	//push
+					out <= {in, out[3:1]};
+					TOS <= in;				
+				end
 endmodule
-
